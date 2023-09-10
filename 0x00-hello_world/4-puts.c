22afa8cd83cd52708a11fca628071cfedafc150d
jsweidan <jawaheryslam@gmail.com>
2023-09-10 16:00:23 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- *main - entry point
+ *main - entry for put function
  *Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-		Return (0);
+		return (0);
 }
