f45f511c1ced4b79aa59598ef8bfae1b4cd6da50
jsweidan <jawaheryslam@gmail.com>
2023-09-11 15:26:39 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in lowercse
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}
