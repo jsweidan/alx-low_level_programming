ca9298df0bc0253b971dfb8b9d978d8a6110386c
jsweidan <jawaheryslam@gmail.com>
2023-09-20 16:45:33 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+ *main - print all numbers base of 10
+ *Description - not allowed to use char
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
