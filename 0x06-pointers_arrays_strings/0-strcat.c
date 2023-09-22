e8f20b1acdf53974147afbe8b172c5d7049909a2
jsweidan <jawaheryslam@gmail.com>
2023-09-22 13:39:09 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,33 @@
 #include "main.h"
 
 /**
-  * _strncat  - the main function
+  *_strcat - main function of the prototype
   *
-  * @n: Function parameter
+  * @src: Concatenation parameter
   *
-  * @src: Function parameter
-  *
-  * @dest: Function parammeter
+  * @dest: Concatenation parameter
   *
   * Return: dest
   */
-char *_strncat(char *dest, char *src, int n)
+
+char *_strcat(char *dest, char *src)
 {
-	int k, r;
+int k, r;
 
-	k = 0;
-	r = 0;
+k = 0;
+r = 0;
 
-	while (dest[k] != '\0')
-	{
+while (dest[k] != '\0')
+{
 	k++;
-	}
-	while (r < n && src[r] != '\0')
-	{
+}
+
+while (src[r] != '\0')
+{
 	dest[k] = src[r];
 	k++;
 	r++;
-	}
-	dest[k] = '\0';
-	return (dest);
+}
+dest[k] = '\0';
+return (dest);
 }
