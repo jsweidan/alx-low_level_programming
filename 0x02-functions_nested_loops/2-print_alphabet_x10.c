7a80f3a21e4f9948f5db2f316b2165e8813dfd86
jsweidan <jawaheryslam@gmail.com>
2023-10-14 14:40:15 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,21 +3,22 @@
 /**
  *print_alphabet_x10 - prints 10 times the alphabet
  *Description: followed by a new line
- *
+ *Return: void
  */
 
 void print_alphabet_x10(void)
 {
 	char i;
-	int j;
+	int j = 0;
 
-	for (j = 0; j < 10; j++)
+	while (j <= 9)
 	{
-		for (i = 'a'; i < 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar (i);
 		}
 
 		_putchar ('\n');
+		j++;
 	}
 }
