9848f252c85cca4374e1c847fe72c4d4d56d3926
jsweidan <jawaheryslam@gmail.com>
2023-10-20 10:11:08 -0700

    macro that computes a sum of numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,15 @@
+#ifndef _SUM_H_
+#define _SUM_H_
+
+/**
+ *main - 4-sum
+ *
+ *@x: the first int input
+ *@y: the second int input
+ *Description: computes the sum of numbers
+ *
+ */
+
+#define SUM(x, y) (x + y)
+
+#endif
