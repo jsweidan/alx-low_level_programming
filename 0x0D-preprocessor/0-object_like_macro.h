f3edeb116a4866cef0224f24a80aa91023c1c9e0
jsweidan <jawaheryslam@gmail.com>
2023-10-20 09:22:20 -0700

    macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _MACRO_H_
+#define _MACRO_H_
+
+/**
+ *File - 0-object_like_macro
+ *
+ *Description: defines a macro named SIZE as an abbreviation for the token 1024
+ */
+
+#define SIZE 1024
+
+#endif
