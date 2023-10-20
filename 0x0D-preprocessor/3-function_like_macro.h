a14211eec07abea61456b05ffc8ecc9a2407c02b
jsweidan <jawaheryslam@gmail.com>
2023-10-20 10:03:01 -0700

    3-function_like_macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+
+/**
+ *File - 3-function_like_macro
+ *
+ *@x: input value
+ *
+ *Description: macroi that computes the absolute value of a number
+ */
+
+#define ABS(x)  ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
