c44eb8627e1833d64e36830737584db15c1c69b2
jsweidan <jawaheryslam@gmail.com>
2023-10-20 10:52:09 -0700

    new struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - creates dog information
+ *
+ *@name: the name of the dog
+ *@age: the age of the dog
+ *@owner: the owner of the dog
+ *
+ *Description: defines a new type
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
