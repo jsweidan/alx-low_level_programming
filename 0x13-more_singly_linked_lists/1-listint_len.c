a94035b723e7ed833e11f170bd97390bbbfd22fa
jsweidan <jawaheryslam@gmail.com>
2023-10-24 13:57:28 -0700

    returns the number of elements in linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that counts the number of elements in a linked list
+ *
+ * @h: header to a pointer
+ *
+ *
+ * Return: the num of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+	for (count = 0; h != NULL; count++)
+	{
+		h = h->next;
+	}
+
+	return (count);
+}
