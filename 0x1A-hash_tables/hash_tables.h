9f1186e47c4117338b70a105baed5a815ce3ce31
jsweidan <jawaheryslam@gmail.com>
2024-07-17 10:58:15 -0700

    a function that creates a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -16,9 +16,9 @@
 
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -32,10 +32,11 @@ typedef struct hash_node_s
 
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
+/*functions*/
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -91,4 +92,4 @@ char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
-#endif /* _HASH_TABLES_H_ */
+#endif /*_HASH_TABLES_H_*/
